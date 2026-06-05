da972e41cede88ec76ba984b33cd420ebbf9b7ca
Aydar Yalaev <a.yalaev@icloud.com>
2026-06-05 17:55:45 +0200

    feat: add task for sprint 6

---
+++ src/Task3/services/service-b/tracing.js
@@ -0,0 +1,13 @@
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+
+const sdk = new NodeSDK({
+    serviceName: 'service-b',
+    traceExporter: new OTLPTraceExporter({
+        url: process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT
+    }),
+    instrumentations: [getNodeAutoInstrumentations()]
+});
+
+sdk.start();
